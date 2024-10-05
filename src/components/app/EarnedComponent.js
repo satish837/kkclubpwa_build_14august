d835c2c90990403e26b842bbf9585f08f2ed60ba
Satish Kumar <kumar.satish837@gmail.com>
2024-10-05 17:06:16 +0530

    code update

--- src/components/app/EarnedComponent.js
+++ src/components/app/EarnedComponent.js
@@ -21,42 +21,49 @@ export default function EarnedComponent() {
     const rewardspoints = TotalrewardpointsComponent();
     const { push } = useRouter();
     const params = useParams()
-    
-    function showDiv() {
-      let div = document.getElementById("fireworks-cont");
-      div.style.display = "block";  // Show the div
-
-      setTimeout(function() {
-        div.style.display = "none";  // Hide the div after 20 seconds
-      }, 9000);  // 20000 milliseconds = 5 seconds
-    }
+   
  
 
     useEffect(() => {
-      console.log(pointnumber);
+      //console.log(pointnumber);
 
       const messageDiv = document.getElementById('message');
+      const fireDiv = document.getElementById('fireworks-cont');
       
       if (pointnumber == 1000) {
         messageDiv.innerHTML = "<h6>Diwali Bonanza Gold Se Growth!</h6><h2>Congratulations</h2><h3>you’ve won <span>₹1000</span> in Gold Se Growth! </h3><h4 class='dw'>Keep scanning for more prizes! </h4>";
-        
+        fireDiv.classList.add("visible");
+        setTimeout(function() {
+          fireDiv.classList.remove("visible");
+      }, 8000);
       } 
       else if (pointnumber == 11) {
         messageDiv.innerHTML = "<h6>Diwali Bonanza Gold Se Growth!</h6><h2>Congratulations</h2><h3>you have won <span>₹5,000</span> worth of Gold Voucher,</h3><h4 class='dw'>a token amount of 11 is credited to your Wallet</h4><h5 class='dw'>Our sales Team will contact you for the delivery of the Gold Voucher.</h5>";
-        showDiv();
+        fireDiv.classList.add("visible");
+        setTimeout(function() {
+          fireDiv.classList.remove("visible");
+      }, 8000);
       }
       else if (pointnumber == 21) {
         messageDiv.innerHTML = "<h6>Diwali Bonanza Gold Se Growth!</h6><h2>Congratulations</h2><h3>you have won <span>₹10,000</span> worth of Gold Voucher,</h3><h4 class='dw'>a token amount of 21 is credited to your Wallet</h4><h5 class='dw'>Our sales Team will contact you for the delivery of the Gold Voucher.</h5>";
-        showDiv();
-        
+        fireDiv.classList.add("visible");
+        setTimeout(function() {
+          fireDiv.classList.remove("visible");
+      }, 8000);
       }
       else if (pointnumber == 51) {
         messageDiv.innerHTML = "<h6>Diwali Bonanza Gold Se Growth!</h6><h2>Congratulations</h2><h3> you have won <span>₹50,000</span> worth of Gold Voucher,</h3><h4 class='dw'>a token amount of 51 is credited to your Wallet</h4><h5 class='dw'>Our sales Team will contact you for the delivery of the Gold Voucher.</h5>";
-        showDiv();
+        fireDiv.classList.add("visible");
+        setTimeout(function() {
+          fireDiv.classList.remove("visible");
+      }, 8000);
       }
       else if (pointnumber == 101) {
         messageDiv.innerHTML = "<h6>Diwali Bonanza Gold Se Growth!</h6><h2>Congratulations</h2><h3> you have won <span>₹1,00,000</span> worth of Gold Voucher, </h3><h4 class='dw'>a token amount of 101 is credited to your Wallet</h4><h5 class='dw'>Our sales Team will contact you for the delivery of the Gold Voucher.</h5>";
-        showDiv();
+        fireDiv.classList.add("visible");
+        setTimeout(function() {
+          fireDiv.classList.remove("visible");
+      }, 8000);
       }
     }, [pointnumber])
     
